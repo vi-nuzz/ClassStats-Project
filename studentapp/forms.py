d2d2d21ef4c172716450149dbddf54dc4d74d57e
Vinayak S <122655109+vi-nuzz@users.noreply.github.com>
2023-04-21 21:07:32 +0530

    Delete studentapp directory

--- studentapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class studentform(forms.Form):
-    student_name = forms.CharField(max_length=30)
-    dateofbirth = forms.DateField()
-    physics = forms.IntegerField()
-    chemistry = forms.IntegerField()
-    maths = forms.IntegerField()
-    computerscience = forms.IntegerField()
--- studentapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('stud_details/',stud_details),
-    path('stud_display/',stud_display),
-]
